c76b33d2251310132aba353fb3b6d65a3c3b9fea
Steve Good <steve.good@sony.com>
2019-06-27 23:54:58 -0700

    Added better help text. Added placeholder commands.

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
@@ -55,10 +56,77 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	}
 
 	if m.Content == "!help" {
-		s.ChannelMessageSend(m.ChannelID, "Sorry, "+m.Author.Mention()+"I can't help you right now since my creator didn't give me any other commands.\nI lied, you can always `!gonk` for fun.")
+		helpText := []string{
+			"!unit <unit card name> - Displays information about the specified unit (WIP)",
+			"!upgrade <upgrade card name> - Displays information about the specified upgrade (WIP)",
+			"!command <command card name> - Displays information about the specified command card (WIP)",
+			"!gonk - GONK",
+			"!help - This help message",
+		}
+		s.ChannelMessageSend(m.ChannelID, "```"+strings.Join(helpText, "\n")+"```")
 	}
 
 	if m.Content == "!gonk" {
 		s.ChannelMessageSend(m.ChannelID, "GONK")
 	}
+
+	if strings.HasPrefix(m.Content, "!unit") {
+		unitName := strings.Replace(m.Content, "!unit", "", 1)
+		unitName = strings.TrimSpace(unitName)
+
+		var response string
+		if len(unitName) == 0 {
+			response = m.Author.Mention() + ", the `!unit` command requires a unit card name. Please try again using this format `!unit <unit card name>`"
+		} else {
+			response = "```" + strings.Join(lookupUnit(unitName), "\n") + "```"
+		}
+
+		s.ChannelMessageSend(m.ChannelID, response)
+	}
+
+	if strings.HasPrefix(m.Content, "!upgrade") {
+		upgradeName := strings.Replace(m.Content, "!upgrade", "", 1)
+		upgradeName = strings.TrimSpace(upgradeName)
+
+		var response string
+		if len(upgradeName) == 0 {
+			response = m.Author.Mention() + ", the `!upgrade` command requires an upgrade card name. Please try again using this format `!upgrade <upgrade card name>`"
+		} else {
+			response = "```" + strings.Join(lookupUpgrade(upgradeName), "\n") + "```"
+		}
+
+		s.ChannelMessageSend(m.ChannelID, response)
+	}
+
+	if strings.HasPrefix(m.Content, "!command") {
+		commandName := strings.Replace(m.Content, "!command", "", 1)
+		commandName = strings.TrimSpace(commandName)
+
+		var response string
+		if len(commandName) == 0 {
+			response = m.Author.Mention() + ", the `!command` command requires a command card name. Please try again using this format `!command <command card name>`"
+		} else {
+			response = "```" + strings.Join(lookupCommand(commandName), "\n") + "```"
+		}
+
+		s.ChannelMessageSend(m.ChannelID, response)
+	}
+}
+
+func lookupUnit(unitName string) []string {
+	cardInfo := []string{"Your mom is " + unitName}
+
+	return cardInfo
+}
+
+func lookupUpgrade(upgradeName string) []string {
+	cardInfo := []string{"Your dad has " + upgradeName}
+
+	return cardInfo
+}
+
+func lookupCommand(commandName string) []string {
+	cardInfo := []string{"In Soviet Russia " + commandName + " plays you!"}
+
+	return cardInfo
 }
