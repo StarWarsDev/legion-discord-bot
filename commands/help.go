0f81af00c1b1b9fcc6167945e7a23a2483b714c8
Stephen Good <stephen.good@umusic.com>
2020-02-25 14:11:04 -0800

    Updated help text

--- commands/help.go
+++ commands/help.go
@@ -9,32 +9,38 @@ import (
 func Help() discordgo.MessageEmbed {
 	fields := []*discordgo.MessageEmbedField{
 		{
-			Name:  "!unit <unit card name>",
-			Value: "Displays the specified unit",
+			Name:  "!unit",
+			Value: "Displays data about Unit cards",
 		},
 		{
-			Name:  "!upgrade <upgrade card name>",
-			Value: "Displays the specified upgrade",
+			Name:  "!upgrade",
+			Value: "Displays data about Upgrade cards",
 		},
 		{
-			Name:  "!command <command card name>",
-			Value: "Displays the specified command card",
+			Name:  "!command",
+			Value: "Displays data about Command cards",
 		},
 		{
-			Name:  "!keyword <keyword name>",
-			Value: "Displays the specified keyword",
+			Name:  "!keyword",
+			Value: "Displays data about Keywords",
 		},
-		//{
-		//	Name:  "!search <search term>",
-		//	Value: "Displays search results across all data",
-		//},
 		{
 			Name:  "!help",
 			Value: "This help message",
 		},
 	}
 
-	info := output.Info("", "")
+	info := output.Info("Help", `**Usage:**
+`+"`!COMMAND [FIELD_NAME] SEARCH_TERM`"+`
+
+`+"`COMMAND` can be any of the available commands below"+`
+`+"`SEARCH_TERM`"+` is treated as a regular expression. All terms are considered _case insensitive_.
+`+"`FIELD_NAME`"+` is optional and defaults to "name" and can be any top level field on the item being queried.
+
+**Example:** `+"`!keyword inspire`"+` finds all keywords with "inspire" in the name.
+**Example 2:** `+"`!keyword description rally`"+` finds all keywords that have the word "rally" in the description.
+
+**Commands:**`)
 
 	info.Fields = fields
 
