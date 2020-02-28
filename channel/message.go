37edb6e6b9dfc4b2382900f38597c5bd73de7648
Stephen Good <stephen.good@umusic.com>
2020-02-28 11:48:41 -0800

    Added mention support. Mentioning the bot will now result in a DM instead of a channel reply.

--- channel/message.go
+++ channel/message.go
@@ -14,8 +14,18 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-func messageSendEmbed(s *discordgo.Session, m *discordgo.MessageCreate, embed *discordgo.MessageEmbed) {
-	_, err := s.ChannelMessageSendEmbed(m.ChannelID, embed)
+func messageSendEmbed(dm bool, s *discordgo.Session, m *discordgo.MessageCreate, embed *discordgo.MessageEmbed) {
+	channelID := m.ChannelID
+	if dm {
+		channel, err := s.UserChannelCreate(m.Author.ID)
+		if err != nil {
+			log.Println("Error creating private channel with user", m.Author.Username, err)
+		} else {
+			channelID = channel.ID
+		}
+	}
+
+	_, err := s.ChannelMessageSendEmbed(channelID, embed)
 	if err != nil {
 		log.Println(err)
 		log.Println("Embedded item as follows")
@@ -30,7 +40,7 @@ func messageSendEmbed(s *discordgo.Session, m *discordgo.MessageCreate, embed *d
 		log.Println(string(b))
 
 		outputError := output.Error("Failed to render", utils.WithTemplate("There was a problem rendering the %s \"%s\"", embed.Author.Name, embed.Title))
-		_, _ = s.ChannelMessageSendEmbed(m.ChannelID, &outputError)
+		_, _ = s.ChannelMessageSendEmbed(channelID, &outputError)
 	}
 }
 
@@ -44,63 +54,43 @@ func NewMessageHandler(client *data.ArchivesClient) interface{} {
 			return
 		}
 
-		// split the message so we can interrogate it for intention
-		cmdParts := strings.Split(m.Content, " ")
-		lenParts := len(cmdParts)
-
-		if lenParts > 0 && strings.HasPrefix(cmdParts[0], "!") {
-			command := cmdParts[0]
-
-			field := ""
-			term := ""
-
-			// if this is not the help command then parse the incoming message
-			if command != "!help" {
-				// by default, assume the field we want to search is "name"
-				field = "name"
-				// set the default term equal to the message body minus the command
-				term = strings.TrimSpace(strings.Replace(m.Content, command, "", -1))
-				args := strings.Split(term, "=")
-				lenArgs := len(args)
-
-				// if there are more than 1 args assume that the search is more complex
-				if lenArgs > 1 {
-					field = args[0]
-					term = strings.TrimSpace(args[1])
-				}
-
-				// clean up the term and field, just in case
-				field = strings.ToLower(strings.TrimSpace(field))
-				term = strings.TrimSpace(term)
-
-				// determine if the term should be a regex
-				_, err := strconv.Atoi(term)
-				if err != nil {
-					// if the term could not be converted to an int then treat is as a regex string
-					term = fmt.Sprintf("(?i)(%s)", term)
-				}
+		content := m.Content
+		isMentioned := false
+		for _, user := range m.Mentions {
+			if user.Mention() == s.State.User.Mention() {
+				isMentioned = true
 			}
+		}
+		if isMentioned {
+			content = m.ContentWithMentionsReplaced()
+			content = strings.ReplaceAll(content, "@"+s.State.User.Username, "")
+			content = strings.TrimSpace(content)
+		}
 
-			log.Println(m.Author.Username+" :", command, field, "=", term)
+		// if the message content starts with an exclamation then it is probably a command
+		if strings.HasPrefix(content, "!") {
+			command, field, term := parseCommandFieldAndTerm(content)
+
+			log.Println(m.Author.Username, ":", command, field, "~", term)
 
 			if command != "!help" && (field == "" || term == "(?i)()") {
 				// this is an invalid command execution, respond with the help command.
 				log.Println("empty field or term detected, this won't do")
-				sendHelp(s, m)
+				sendHelp(isMentioned, s, m)
 				// stop trying to process the command
 				return
 			}
 
 			switch command {
 			case "!help":
-				sendHelp(s, m)
+				sendHelp(isMentioned, s, m)
 			case "!keyword":
 				// get the keywords from the archives client
 				keywords := client.GetKeywords(field, term)
 				// for each result, send an embedded response message
 				for _, keyword := range keywords {
 					response := commands.Keyword(&keyword)
-					messageSendEmbed(s, m, &response)
+					messageSendEmbed(isMentioned, s, m, &response)
 				}
 			case "!command":
 				// get the command cards from the archives client
@@ -108,7 +98,7 @@ func NewMessageHandler(client *data.ArchivesClient) interface{} {
 				// for each result, send an embedded response message
 				for _, card := range commandCards {
 					response := commands.Command(&card)
-					messageSendEmbed(s, m, &response)
+					messageSendEmbed(isMentioned, s, m, &response)
 				}
 			}
 
@@ -159,7 +149,43 @@ func NewMessageHandler(client *data.ArchivesClient) interface{} {
 	return handler
 }
 
-func sendHelp(s *discordgo.Session, m *discordgo.MessageCreate) {
+func parseCommandFieldAndTerm(content string) (string, string, string) {
+	cmdParts := strings.Split(content, " ")
+	command := cmdParts[0]
+
+	field := ""
+	term := ""
+
+	// if this is not the help command then parse the incoming message
+	if command != "!help" {
+		// by default, assume the field we want to search is "name"
+		field = "name"
+		// set the default term equal to the message body minus the command
+		term = strings.TrimSpace(strings.Replace(content, command, "", -1))
+		args := strings.Split(term, "~")
+		lenArgs := len(args)
+
+		// if there are more than 1 args assume that the search is more complex
+		if lenArgs > 1 {
+			field = args[0]
+			term = strings.TrimSpace(args[1])
+		}
+
+		// clean up the term and field, just in case
+		field = strings.ToLower(strings.TrimSpace(field))
+		term = strings.TrimSpace(term)
+
+		// determine if the term should be a regex
+		_, err := strconv.Atoi(term)
+		if err != nil {
+			// if the term could not be converted to an int then treat is as a regex string
+			term = fmt.Sprintf("(?i)(%s)", term)
+		}
+	}
+	return command, field, term
+}
+
+func sendHelp(dm bool, s *discordgo.Session, m *discordgo.MessageCreate) {
 	helpContent := commands.Help()
-	messageSendEmbed(s, m, &helpContent)
+	messageSendEmbed(dm, s, m, &helpContent)
 }
--- commands/help.go
+++ commands/help.go
@@ -29,16 +29,19 @@ func Help() discordgo.MessageEmbed {
 			Value: "This help message",
 		},
 	}
-
 	info := output.Info("Help", `**Usage:**
-`+"`!COMMAND [FIELD_NAME =] SEARCH_TERM`"+`
+`+"`!COMMAND [FIELD_NAME ~] SEARCH_TERM`"+`
 
 `+"`COMMAND` can be any of the available commands below"+`
-`+"`SEARCH_TERM`"+` is treated as a regular expression. All terms are considered _case insensitive_.
 `+"`FIELD_NAME`"+` is optional and defaults to "name" and can be any top level field on the item being queried.
+`+"`SEARCH_TERM`"+` is treated as a regular expression. All terms are considered _case insensitive_.
 
 **Example:** `+"`!keyword inspire`"+` finds all keywords with "inspire" in the name.
-**Example 2:** `+"`!keyword description = rally`"+` finds all keywords that have the word "rally" in the description.
+
+**Example 2:** `+"`!keyword description ~ rally`"+` finds all keywords that have the word "rally" in the description.
+
+**DM**: By default the bot will respond in the channel where the command was issued.
+If you like to have the bot send you a DM instead just mention it anywhere in the message.
 
 **Commands:**`)
 
